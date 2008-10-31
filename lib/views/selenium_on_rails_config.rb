8b32c23292b49f606b89faac4184a22ba2c4215b
Tapajós <tapajos@gmail.com>
2008-10-30 22:43:58 -0200

    Now you can use Ruby to configure you config.yml like in Rails fixtures.
    Example:
    
    host: <%=`hostname`.gsub(/\n/, '')%>
    port_start: 4000
    port_end: 4001
    max_browser_duration: 1440

--- lib/views/selenium_on_rails_config.rb
+++ lib/views/selenium_on_rails_config.rb
@@ -13,8 +13,12 @@ class SeleniumOnRailsConfig
   private
     def self.configs
       unless defined? @@configs
-        file = File.expand_path(File.dirname(__FILE__) + '/../config.yml')
-        @@configs = File.exist?(file) ? YAML.load_file(file) : {}
+        files = [File.expand_path(File.dirname(__FILE__) + '/../config.yml')]
+        files << File.join(RAILS_ROOT, 'config', 'selenium.yml')
+        files.each do |file|
+          @@configs = YAML.load(ERB.new(IO.read(file)).result) if File.exist?(file)
+        end
+        @@configs ||= {}
       end
       @@configs
     end
